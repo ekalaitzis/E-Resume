ee26298f23546e4686482dc08be585615c11347e
Ace <kalaitzisemmanouil@gmail.com>
2024-11-03 13:18:04 +0100

    fix for test profile

--- src/test/java/dev/manos/E_Resume/EResumeApplicationTests.java
+++ src/test/java/dev/manos/E_Resume/EResumeApplicationTests.java
@@ -3,13 +3,32 @@ package dev.manos.E_Resume;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-@Import(TestcontainersConfiguration.class)
 @SpringBootTest
+@Testcontainers
+@ActiveProfiles("test")
 class EResumeApplicationTests {
+
+	@Container
+	static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15-alpine")
+			.withDatabaseName("test_db")
+			.withUsername("test")
+			.withPassword("test");
+
+	@DynamicPropertySource
+	static void configureProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.datasource.url", postgres::getJdbcUrl);
+		registry.add("spring.datasource.username", postgres::getUsername);
+		registry.add("spring.datasource.password", postgres::getPassword);
+	}
 
 	@Test
 	void contextLoads() {
 	}
-
 }
